4b5b8e2edd6a152edb9711a2389114340add0aea
Blake Strickland <Stricklandb02@gmail.com>
2015-09-25 16:50:17 -0400

    Move everything to orginization

---
+++ test/models/current_condition_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+# class RainTest
+#   private def get_response
+#     file = File.join(Rails.root, 'test','models','json_weather','rain_weather.json')
+#     File.read(file)
+#     #data_hash = JSON.parse(file)
+#   end
+# end
+
+class CurrentConditionModelTest < ActiveSupport::TestCase
+
+  def test_condition_at_location
+    assert_equal "Rain", CurrentCondition.new("29115").condition
+  end
+
+end
